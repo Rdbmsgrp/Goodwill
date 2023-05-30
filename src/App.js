77e7fece6063b5026d441f3f0f089bdbaad43189
Jayalakshmi P.E <pejayalakshmi2003@gmail.com>
2023-05-30 16:12:43 +0530

    login created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route ,Routes } from 'react-router-dom';
 import Login from './Pages/Login';
 import Landing from './Pages/Landing';
+import Login1 from './Pages/Login1';
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" Component={Landing}/>
           <Route path="/login" Component={Login}/>
-        
+          <Route path="/Login1" Component={Login1}/>
         </Routes>
       </BrowserRouter>
     </div>
